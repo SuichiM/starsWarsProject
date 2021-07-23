ac506529f1add4ccc46b421aa0d0d123cc4e6f90
SuichiM <MartiniRoqueEsteban@gmail.com>
2021-07-22 21:29:14 -0300

    env in the right way

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,15 +1,19 @@
 import path from 'path';
-import {Configuration, DefinePlugin} from 'webpack';
+import {Configuration, DefinePlugin, HotModuleReplacementPlugin} from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
 import TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin';
 
+const {config: configDotEnv} = require('dotenv');
+const dotenv = configDotEnv();
+
+const isDev = process.env.production || !process.env.development;
+const HOST = process.env.HOST || '0.0.0.0';
+const PORT = process.env.PORT ? parseInt(process.env.PORT) : 8080;
+
 const webpackConfig = (): Configuration => ({
   entry: './src/index.tsx',
-  ...(process.env.production || !process.env.development
-    ? {}
-    : {devtool: 'eval-source-map'}),
-
+  ...(isDev ? {devtool: 'eval-source-map'} : {}),
   resolve: {
     extensions: ['.ts', '.tsx', '.js'],
     plugins: [new TsconfigPathsPlugin({configFile: './tsconfig.json'})],
@@ -35,18 +39,19 @@ const webpackConfig = (): Configuration => ({
     ],
   },
   devServer: {
-    port: 6666,
+    host: HOST,
+    port: PORT,
     open: true,
     historyApiFallback: true,
   },
   plugins: [
+    new HotModuleReplacementPlugin(),
     new HtmlWebpackPlugin({
       // HtmlWebpackPlugin simplifies creation of HTML files to serve your webpack bundles
       template: './public/index.html',
     }),
-    // DefinePlugin allows you to create global constants which can be configured at compile time
     new DefinePlugin({
-      'process.env': process.env.production || !process.env.development,
+      'process.env': `(${JSON.stringify(dotenv.parsed)})`,
     }),
     new ForkTsCheckerWebpackPlugin({
       // Speeds up TypeScript type checking and ESLint linting (by moving each to a separate process)
