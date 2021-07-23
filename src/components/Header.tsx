96e528cb8c07d850a076a4efe81759a28aca1f3f
SuichiM <MartiniRoqueEsteban@gmail.com>
2021-07-23 13:35:59 -0300

    editing the HEADER

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,10 +11,8 @@ const PageHeader: React.FC = () => (
       width: '100%',
       textAlign: 'center',
     }}>
-    <h1 className="mt-2 text-white d-none d-md-block">
-      THE STARS WARS CHALLENGE
-    </h1>
-    <h1 className="mt-2 text-white d-block d-md-none">THE SWC</h1>
+    <h1 className="mt-2 text-white d-none d-md-block">THIS IS THE PR 1</h1>
+    <h1 className="mt-2 text-white d-block d-md-none"> THIS IS THE PR 1</h1>
   </Header>
 );
 
